d41eaf780273185488deb85f72431b88d610d253
Matheus Mangualde <matheusmangualde@gmail.com>
2025-11-17 14:37:20 -0300

    feat: update CORS configuration to restrict allowed origins

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	r := gin.Default()
 
 	corsConfig := cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"https://relaunch-cot.netlify.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
